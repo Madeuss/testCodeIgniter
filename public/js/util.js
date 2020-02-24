534146ad68df754ea8a08a09027c47e47824069b
Madeuss <mateus1two3@gmail.com>
2020-02-24 20:00:29 -0300

    Criação pasta public

---
+++ public/js/util.js
@@ -0,0 +1,19 @@
+const BASE_URL = "localhost/dattebayo/";
+
+function clearErrors() {
+    $(".has-error").removeClass("has-error");
+    $(".help-block").html("");
+}
+
+function showError(error_list) {
+    clearErrors();
+
+    $.each(error_list, function(id, message) {
+        $(id).parent().parent().addClass("has-error");
+        $(id).parent().siblings(".alert-danger").html(message);
+    })
+}
+
+function loadindImg(message="") {
+    return "<i class='fa fa-circle-o-notch fa-spin'></i>&nbsp;" + message;
+}
